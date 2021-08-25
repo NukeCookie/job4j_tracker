1528104bfd2d1e7d35f7c42debd41231cde6c5cf
NukiCoockie <nimenithewho@gmail.com>
2021-08-25 18:22:34 +0500

    1.6. Взаимодействие объектов [#166087]

---
+++ src/main/java/ru/job4j/oop/kolobok/Ball.java
@@ -0,0 +1,10 @@
+package ru.job4j.oop.kolobok;
+
+public class Ball {
+    public void escape(Hare hare) {
+    }
+    public void escape(Wolf wolf) {
+    }
+    public void eaten(Fox fox) {
+    }
+}
