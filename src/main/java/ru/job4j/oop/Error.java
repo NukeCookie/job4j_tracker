458535f9b7fceaa27d1def0fd3fd4adbd5efb4f1
NukiCoockie <nimenithewho@gmail.com>
2021-09-14 15:11:22 +0500

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,27 @@
+package ru.job4j.oop;
+
+public class Error {
+    private boolean active;
+    private int status;
+    private String message;
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+    public Error() {
+    }
+
+public static void main(String[] args) {
+        Error error = new Error(true, 2, "ok");
+        error.printInfo();
+        Error er = new Error();
+        er.printInfo();
+    }
+
+    public void printInfo() {
+        System.out.println(active);
+        System.out.println(status);
+        System.out.println(message);
+    }
+}
