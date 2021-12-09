fec849a4e031b615826dbde6d2ef8b05a78ba046
NukeCookie <nimenithewho@gmail.com>
2021-12-10 02:32:41 +0500

    6. Date. Отображение даты. [#310017]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,8 +1,12 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Item {
     private int id;
     private String name;
+    private LocalDateTime created = LocalDateTime.now();
 
     public Item() {
     }
@@ -11,9 +15,10 @@ public class Item {
         this.name = name;
     }
 
-    public Item(int id, String name) {
+    public Item(int id, String name, LocalDateTime created) {
         this.id = id;
         this.name = name;
+        this.created = created.now();
     }
 
     public int getId() {
@@ -30,5 +35,9 @@ public class Item {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
     }
 }
