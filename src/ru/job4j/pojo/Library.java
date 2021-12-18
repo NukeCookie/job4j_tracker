acd629e38984cb51944bee2f5f24e4774a3810a4
NukeCookie <nimenithewho@gmail.com>
2021-12-18 17:25:55 +0500

    3. Массивы и модели. [#174892 #241095]

--- src/ru/job4j/pojo/Library.java
+++ src/ru/job4j/pojo/Library.java
@@ -26,7 +26,7 @@ public class Library {
         System.out.println("Вывод книг с именем \"Clean code\".");
         for (int index = 0; index < books.length; index++) {
             Book bk = books[index];
-            if (bk.getTitle().equals("Clean code")) {
+            if ("Clean code".equals(bk.getTitle())) {
                 System.out.println(bk.getTitle() + " - " + bk.getPages() + " стр.");
             }
         }
