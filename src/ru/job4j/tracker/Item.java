cd85f2cb35d9b733086dad27103bade1b6040d14
NukeCookie <nimenithewho@gmail.com>
2021-12-23 22:57:58 +0500

    5. Tracker - хранилище [#396]

--- src/ru/job4j/tracker/Item.java
+++ src/ru/job4j/tracker/Item.java
@@ -1,11 +1,12 @@
 package ru.job4j.tracker;
 
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 public class Item {
     private int id;
+
     private String name;
+
     private LocalDateTime created = LocalDateTime.now();
 
     public Item() {
@@ -15,10 +16,9 @@ public class Item {
         this.name = name;
     }
 
-    public Item(int id, String name, LocalDateTime created) {
+    public Item(int id, String name) {
         this.id = id;
         this.name = name;
-        this.created = created.now();
     }
 
     public int getId() {
--- src/ru/job4j/tracker/Tracker.java
+++ src/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -21,5 +23,31 @@ public class Tracker {
             }
         }
         return rsl;
+    }
+
+    public Item[] findAll() {
+        Item[] itemsWithoutNull = new Item[items.length];
+        int count = 0;
+        for (int index = 0; index < items.length; index++) {
+            if (items[index] != null) {
+                itemsWithoutNull[count] = items[index];
+                count++;
+            }
+        }
+        return Arrays.copyOf(itemsWithoutNull, count);
+    }
+
+    public Item[] findByName(String key) {
+        Item[] rsl = new Item[items.length];
+        int count = 0;
+        for (int i = 0; i < items.length; i++) {
+            if (items[i] != null) {
+                if (items[i].getName().equals(key)) {
+                    rsl[count] = items[i];
+                    count++;
+                }
+            }
+        }
+        return Arrays.copyOf(rsl, count);
     }
 }
