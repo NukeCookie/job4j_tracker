8dc7b77b73069daabd84d93449bc95c0d2252315
NukeCookie <nimenithewho@gmail.com>
2022-04-25 14:50:54 +0300

    0.4. Пользовательские исключения. [#219367 #279086]

--- src/ru/job4j/ex/FindEl.java
+++ src/ru/job4j/ex/FindEl.java
@@ -3,9 +3,10 @@ package ru.job4j.ex;
 public class FindEl {
     public static int indexOf(String[] value, String key) throws ElementNotFoundException {
         int rsl = -1;
-        for (int i = 0; i < value.length - 1; i++) {
+        for (int i = 0; i < value.length; i++) {
             if (value[i].equals(key)) {
                 rsl = i;
+                break;
             }
         }
         if (rsl == -1) {
