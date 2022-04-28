a868b9daa1b9cc9174e336169a4c751453b2b50b
NukeCookie <nimenithewho@gmail.com>
2022-04-28 12:42:46 +0300

    err

--- src/ru/job4j/ex/UserStore.java
+++ src/ru/job4j/ex/UserStore.java
@@ -18,7 +18,7 @@ public class UserStore {
     }
 
     public static boolean validate(User user) throws UserInvalidException {
-        if (!user.isValid() || user.getUsername().length() < 3) {
+        if (user.isValid() && user.getUsername().length() >= 3) {
             return true;
         } else {
             throw new UserInvalidException("User is not valid");
