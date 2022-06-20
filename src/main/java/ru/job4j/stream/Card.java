f0f2e264926fa1cc96a2410ec515e25f66f1e6ac
NukeCookie <nimenithewho@gmail.com>
2022-06-20 18:44:16 +0300

    8. Перенос методов в Stream API [#279131 #314312]

--- src/main/java/ru/job4j/stream/Card.java
+++ src/main/java/ru/job4j/stream/Card.java
@@ -11,10 +11,18 @@ public class Card {
         this.value = value;
     }
 
+    @Override
+    public String toString() {
+        return "Card{"
+                + "suit=" + suit
+                + ", value=" + value
+                + '}';
+    }
+
     public static void main(String[] args) {
         Stream.of(Suit.values())
                 .flatMap(suit1 -> Stream.of(Value.values())
-                        .map(value -> "Suit: " + suit1 + ", value: " + value))
+                        .map(value -> new Card(suit1, value)))
                 .forEach(System.out::println);
 
     }
